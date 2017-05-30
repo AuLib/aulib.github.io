92705cf9995a585fe964a0d3fa552a3fd7bcdee4
veplaini <victor.lazzarini@nuim.ie>
2017-05-30 15:38:45 +0100

    first commit

---
+++ search/classes_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['midiin',['MidiIn',['../class_au_lib_1_1_midi_in.html',1,'AuLib']]]
+];
---
+++ search/files_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['midiin_2ecpp',['MidiIn.cpp',['../_midi_in_8cpp.html',1,'']]],
+  ['midiin_2eh',['MidiIn.h',['../_midi_in_8h.html',1,'']]]
+];
